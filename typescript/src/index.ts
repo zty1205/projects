b9b30d38ad6a7199fc2ab06ac48dfa186c6ba3e9
zty1205 <zty1160051490@163.com>
2022-03-28 15:35:12 +0800

    feat: ts 接口

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,8 @@
-console.log('typescript');
+import { Person1 } from './modules/interface';
+
+// 接口
+let p1 = new Person1({
+  name: 'zty',
+  id: 123456
+});
+console.log('p1 speak: ', p1.speak());
